479f7f5c53b94d61401f4896e9ec9fcf06305e70
Molokova Rita <Molok_cv58@student.itstep.org>
2024-09-25 12:33:05 +0300

    Добавьте файлы проекта.

---
+++ hwDynamicMemory/hwDynamicMemory.cpp
@@ -0,0 +1,30 @@
+﻿using namespace std;
+#include <iostream>
+#include <random>
+
+int main()
+{
+    srand(time(0));
+    int size;
+    cout << "Enter size: ";
+    cin >> size;
+    int* ptr = new int[size];
+    for (int i = 0; i < size; i++) {
+        ptr[i] = 1 + rand() % (100 - 1 + 1);
+        cout << ptr[i] << "\t";
+    }
+    cout << endl;
+    int* ptr2 = new int[size + 1];
+    for (int i = 0; i < size; i++) {
+        ptr2[i] = ptr[i];
+    }
+    ptr2[size] = 100;
+    for (int i = 0; i <= size; i++) {
+        cout << ptr2[i] << "\t";
+    }
+    cout << endl;
+    delete[] ptr;
+    delete[] ptr2;
+}
+
+
